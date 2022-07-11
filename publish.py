4919e07a8bb41a6726a81fcf0ac7ec54ba729b03
hlf20010508 <hlf01@icloud.com>
2022-07-11 14:27:43 +0800

    fix name problem in publish.py

--- publish.py
+++ publish.py
@@ -19,9 +19,9 @@ import minio_async
 os.system('python setup.py sdist')
 os.system('twine upload dist/miniopy-async-%s.tar.gz' %
           minio_async.__version__)
-for file in os.listdir('minio_async.egg-info'):
-    os.remove(os.path.join('minio_async.egg-info', file))
-os.rmdir('minio_async.egg-info')
+for file in os.listdir('miniopy_async.egg-info'):
+    os.remove(os.path.join('miniopy_async.egg-info', file))
+os.rmdir('miniopy_async.egg-info')
 for file in os.listdir('dist'):
     os.remove(os.path.join('dist', file))
 os.rmdir('dist')
